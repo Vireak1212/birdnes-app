2fe589abeb648c554ee6644b710c03c8ff85e1cc
SiengTanglim <siengtanglim@gmail.com>
2021-03-04 11:23:57 +0700

    Update

--- src/components/ProfileScreen.tsx
+++ src/components/ProfileScreen.tsx
@@ -26,11 +26,13 @@ const ProfileScreen = () => {
                 leftIcon={leftIcon()}
             />
             <ScrollView>
-                <TouchableOpacity>
+                <TouchableOpacity style={{ alignItems: 'center' }}>
                     <View style={{
                         marginTop: 25,
                         alignItems: 'center',
                         justifyContent: 'center',
+                        width: 100,
+
                     }}>
                         <Image style={{
                             height: 100,
@@ -41,7 +43,7 @@ const ProfileScreen = () => {
                             resizeMethod="auto"
                             resizeMode="cover"
                         />
-                        <View style={{ position: 'absolute', bottom: 10, right: 120 }}>
+                        <View style={{ position: 'absolute', bottom: 0, right: 2 }}>
                             <View style={{
                                 backgroundColor: '#fff', height: 30, width: 30, borderRadius: 20, alignItems: 'center',
                                 justifyContent: 'center'
@@ -51,72 +53,74 @@ const ProfileScreen = () => {
                         </View>
                     </View>
                 </TouchableOpacity>
-                <View style={{ marginTop: 60, backgroundColor: '#fff', height: 240, width: '100%' }}>
-                    <View style={{ margin: 10 }}>
-                        <Text style={{ fontWeight: 'bold', fontSize: 20 }}>Account Info</Text>
-                        <Text style={{ opacity: 0.4, marginTop: 10 }}>Information about your dransaction</Text>
-                        <TouchableOpacity>
-                            <View style={{ marginTop: 15, flexDirection: 'row' }}>
-                                <View>
-                                    <Fontisto name='play-list' size={25} style={{ marginTop: 15, opacity: 0.6 }} color='#000' />
+                <View style={{ marginHorizontal: 5 }}>
+                    <View style={{ marginTop: 60, backgroundColor: '#fff', height: 240, width: '100%' }}>
+                        <View style={{ margin: 10 }}>
+                            <Text style={{ fontWeight: 'bold', fontSize: 20 }}>Account Info</Text>
+                            <Text style={{ opacity: 0.4, marginTop: 10 }}>Information about your dransaction</Text>
+                            <TouchableOpacity>
+                                <View style={{ marginTop: 15, flexDirection: 'row' }}>
+                                    <View>
+                                        <Fontisto name='play-list' size={25} style={{ marginTop: 15, opacity: 0.6 }} color='#000' />
+                                    </View>
+                                    <View style={{ marginLeft: 20 }}>
+                                        <Text style={{ fontSize: 18 }}>Orders</Text>
+                                        <Text style={{ marginTop: 10, opacity: 0.4 }}>View your order informations</Text>
+                                    </View>
                                 </View>
-                                <View style={{ marginLeft: 20 }}>
-                                    <Text style={{ fontSize: 18 }}>Orders</Text>
-                                    <Text style={{ marginTop: 10, opacity: 0.4 }}>View your order informations</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity>
+                                <View style={{ flexDirection: 'row', marginTop: 20 }}>
+                                    <View>
+                                        <AntDesign name='star' size={30} style={{ marginTop: 15, opacity: 0.6 }} color='#000' />
+                                    </View>
+                                    <View style={{ marginLeft: 20 }}>
+                                        <Text>Wishlish</Text>
+                                        <Text style={{ marginTop: 15, opacity: 0.4 }}>All your favorite wishlist</Text>
+                                    </View>
                                 </View>
-                            </View>
-                        </TouchableOpacity>
-                        <TouchableOpacity>
-                            <View style={{ flexDirection: 'row', marginTop: 20 }}>
-                                <View>
-                                    <AntDesign name='star' size={30} style={{ marginTop: 15, opacity: 0.6 }} color='#000' />
-                                </View>
-                                <View style={{ marginLeft: 20 }}>
-                                    <Text>Wishlish</Text>
-                                    <Text style={{ marginTop: 15, opacity: 0.4 }}>All your favorite wishlist</Text>
-                                </View>
-                            </View>
-                        </TouchableOpacity>
+                            </TouchableOpacity>
+                        </View>
                     </View>
-                </View>
-                <View style={{ marginTop: 15, backgroundColor: '#fff', height: 330, width: '100%', marginBottom: 50 }}>
-                    <View style={{ margin: 10 }}>
-                        <Text style={{ fontWeight: 'bold', fontSize: 20 }}>Account Settings</Text>
-                        <Text style={{ opacity: 0.4, marginTop: 10 }}>
-                            Manage information about, your payments and your contacts, and your account in general.</Text>
-                        <TouchableOpacity onPress={() => navigate.navigate('editProfile')}>
-                            <View style={{ marginTop: 15, flexDirection: 'row' }}>
-                                <View>
-                                    <FontAwesome5 name='user-cog' size={25} style={{ marginTop: 15, opacity: 0.6 }} color='#000' />
+                    <View style={{ marginTop: 15, backgroundColor: '#fff', height: 350, width: '100%' }}>
+                        <View style={{ margin: 10 }}>
+                            <Text style={{ fontWeight: 'bold', fontSize: 20 }}>Account Settings</Text>
+                            <Text style={{ opacity: 0.4, marginTop: 10, width: '100%' }}>
+                                Manage information about, your payments and your contacts, and your account in general.</Text>
+                            <TouchableOpacity onPress={() => navigate.navigate('editProfile')}>
+                                <View style={{ marginTop: 15, flexDirection: 'row' }}>
+                                    <View>
+                                        <FontAwesome5 name='user-cog' size={25} style={{ marginTop: 15, opacity: 0.6 }} color='#000' />
+                                    </View>
+                                    <View style={{ marginLeft: 20 }}>
+                                        <Text>Update Information</Text>
+                                        <Text style={{ marginTop: 15, opacity: 0.4 }}>
+                                            Update your name, phone number and email address</Text>
+                                    </View>
                                 </View>
-                                <View style={{ marginLeft: 20 }}>
-                                    <Text>Update Information</Text>
-                                    <Text style={{ marginTop: 15, opacity: 0.4, width: '100%' }}>Update your name, phone number and email add...</Text>
-                                </View>
-                            </View>
-                        </TouchableOpacity>
-                        <TouchableOpacity onPress={() => navigate.navigate('shippingAdress')}>
-                            <View style={{ marginTop: 30, flexDirection: 'row' }}>
-                                <View>
-                                    <FontAwesome5 name='car-side' size={25} style={{ marginTop: 20, opacity: 0.6 }} color='#000' />
-                                </View>
-                                <View style={{ marginLeft: 20 }}>
-                                    <Text>
-                                        Shipping Address
+                            </TouchableOpacity>
+                            <TouchableOpacity onPress={() => navigate.navigate('shippingAdress')}>
+                                <View style={{ marginTop: 30, flexDirection: 'row' }}>
+                                    <View>
+                                        <FontAwesome5 name='car-side' size={25} style={{ marginTop: 20, opacity: 0.6 }} color='#000' />
+                                    </View>
+                                    <View style={{ marginLeft: 20 }}>
+                                        <Text>
+                                            Shipping Address
                                     </Text>
-                                    <Text style={{ marginTop: 15, opacity: 0.4 }}>
-                                        Make Update to your current address
+                                        <Text style={{ marginTop: 15, opacity: 0.4 }}>
+                                            Make Update to your current address
                                     </Text>
+                                    </View>
                                 </View>
-                            </View>
-                        </TouchableOpacity>
-                        <TouchableOpacity>
-                            <View style={{ backgroundColor: '#224889', height: 50, marginTop: 20, justifyContent: 'center', alignItems: 'center', borderRadius: 15 }}>
-                                <Text style={{ color: '#fff' }}>Sign Out</Text>
-                            </View>
-                        </TouchableOpacity>
+                            </TouchableOpacity>
+                            <TouchableOpacity>
+                                <View style={{ backgroundColor: '#224889', height: 50, marginTop: 20, justifyContent: 'center', alignItems: 'center', borderRadius: 15 }}>
+                                    <Text style={{ color: '#fff' }}>Sign Out</Text>
+                                </View>
+                            </TouchableOpacity>
+                        </View>
                     </View>
-
                 </View>
 
             </ScrollView>
